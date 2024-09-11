41c33566e9ff19c911dc4a9ef2ae146badf1a8d7
Cheokerinos <cheokbryan@gmail.com>
2024-09-11 17:24:51 +0800

    done with random number generator.

--- Project_1/test.c
+++ Project_1/test.c
@@ -1,7 +1,24 @@
 #include <stdio.h>
-#include <malloc.h>
-#include <sorting.h>
+//#include <malloc.h>
+//#include <sorting.h>
+#include <stdlib.h>
+#include <time.h>
+
+
+void gen_random_numbers(int *array, int len, int min, int max){
+    for (int i = 0; i < len; i++)
+        array[i] = rand() % (max - min + 1) + min;
+}
 
 int main() {
-    int x = 0;
+    srand(time(0));
+    int Size = 100;
+    int *array = (int *) malloc(Size * sizeof(int));
+    gen_random_numbers(array,Size,1,100000);
+
+    for(int i=0;i<Size;i++){
+        printf("%d ", array[i]);
+    }
+    free(array);
+    return 0;
 }
